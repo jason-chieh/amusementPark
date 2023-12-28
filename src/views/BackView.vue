22ce6123d3a82113155db601f2e4f24a439cab95
xiachuan945 <j5577919@gmail.com>
2023-12-28 09:50:40 +0800

    12-28 temporary branch

--- src/views/BackView.vue
+++ src/views/BackView.vue
@@ -455,11 +455,7 @@ export default {
                 return
             }
 
-<<<<<<< HEAD
-            //判斷你是不是超級管理員
-=======
             判斷你是不是超級管理員
->>>>>>> 12-27_restaurant_fetch_DB
             if (this.loginInfo.adminuser.account != "superadmin") {
                 //確定你有沒有權利
                 if (this.loginInfo.adminuser.managePlace != this.region || this.loginInfo.adminuser.manageNum < 20) {
@@ -467,10 +463,6 @@ export default {
                     return
                 }
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> 12-27_restaurant_fetch_DB
 
             var url = "http://localhost:8080/api/park/create";
             var data = {
@@ -642,11 +634,8 @@ export default {
                     return
                 }
             }
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 12-27_restaurant_fetch_DB
 
             //後端先
             console.log(this.allFacility[index].name)
@@ -699,10 +688,6 @@ export default {
                     return
                 }
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> 12-27_restaurant_fetch_DB
 
             const url = 'http://localhost:8080/api/park/updateFacility';
             // 要帶入的值
@@ -1221,10 +1206,6 @@ export default {
                     return
                 }
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> 12-27_restaurant_fetch_DB
             const url = 'http://localhost:8080/api/park/updateRestaurant';
 
             // 要带入的值
@@ -1383,8 +1364,8 @@ export default {
                                 index="3-1">管理餐廳</el-menu-item>
                             <el-menu-item @click="goAddRestaurant" style="cursor: pointer;" class="child"
                                 index="3-2">新增餐廳</el-menu-item>
-                            <el-menu-item @click="goEditRestaurant" style="cursor: pointer;" class="child"
-                                index="3-3">修改餐廳</el-menu-item>
+                            <!-- <el-menu-item @click="goEditRestaurant" style="cursor: pointer;" class="child"
+                                index="3-3">修改餐廳</el-menu-item> -->
                         </el-sub-menu>
 
 
